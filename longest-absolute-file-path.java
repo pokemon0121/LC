9cb698bbe90b28fafe55ce8a006e8c1ef6f5865d
pokemon0121 <Delphox0121@gmail.com>
2017-11-21 18:43:39 -0800

    test commit !!!

--- longest-absolute-file-path.java
+++ longest-absolute-file-path.java
@@ -1,1 +1,17 @@
-Updated/Created at Tue Nov 21 2017 18:32:02 GMT-0800 (PST).
+public class Solution {
+    public int lengthLongestPath(String input) {
+        String[] paths = input.split("\n");
+        int[] depth = new int[paths.length + 1];
+        int maxLen = 0;
+        for (String s : paths) {
+            int level = s.lastIndexOf("\t") + 1;
+            depth[level + 1] = depth[level] + s.length() - level + 1;
+            int curLen = depth[level + 1];
+            if (s.contains(".")) {
+                maxLen = Math.max(maxLen, curLen - 1);
+            }
+        }
+        return maxLen;
+    }
+}
+​
