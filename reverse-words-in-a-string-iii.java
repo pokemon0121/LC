510627128141cc0d345df546485edfe5d7878119
pokemon0121 <Delphox0121@gmail.com>
2017-12-21 23:23:27 -0800

    auto-update-file commit

--- reverse-words-in-a-string-iii.java
+++ reverse-words-in-a-string-iii.java
@@ -1,1 +1,14 @@
-test-string
+public class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        String r = "";
+        for (int i = 0; i < words.length; i++) {
+            StringBuilder sb = new StringBuilder(words[i]);
+            sb.reverse();
+            r += sb.toString();
+            r += i == words.length - 1 ? "" : " ";
+        }
+        return r;
+    }
+}
+​
