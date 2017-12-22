758b559ec79f9b79f02390143e23b0e050fc2eaf
pokemon0121 <Delphox0121@gmail.com>
2017-12-22 10:01:59 -0800

    auto-create-file commit

---
+++ find-all-numbers-disappeared-in-an-array.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            // if it is there, make it negtive 
+            if (nums[Math.abs(nums[i]) - 1] > 0) {
+                nums[Math.abs(nums[i]) - 1] = -nums[Math.abs(nums[i]) - 1];
+            }
+        }
+        List<Integer> r = new ArrayList<>();
+        // remaining positive ones are answers
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                r.add(i + 1);
+            }
+        }
+        return r;
+    }
+}
+​
