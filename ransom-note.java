9f6e1a0cfd2b5b5079752c1901e9b4862ab38915
pokemon0121 <Delphox0121@gmail.com>
2017-12-22 11:08:15 -0800

    auto-create-file commit

---
+++ ransom-note.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[128];
+        for (char c : magazine.toCharArray()) count[c]++;
+        for (char c : ransomNote.toCharArray())
+            if (count[c]-- <= 0) return false;
+        return true;
+    }
+}
+​
