6114ca0729d8280da6f732201ac6bb002009fa83
pokemon0121 <Delphox0121@gmail.com>
2017-12-21 23:53:24 -0800

    auto-update-file commit

--- reshape-the-matrix.java
+++ reshape-the-matrix.java
@@ -1,1 +1,23 @@
-Updated/Created at Tue Nov 21 2017 16:01:26 GMT-0800 (PST).
+public class Solution {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        int m = nums.length;
+        int n = nums[0].length;
+        if (m * n != r * c) {
+            return nums;
+        }
+        int[][] res = new int[r][c];
+        int ii = 0, jj = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[ii][jj] = nums[i][j];
+                jj++;
+                if (jj == c) {
+                    jj = 0;
+                    ii++;
+                }
+            }
+        }
+        return res;
+    }
+}
+​
