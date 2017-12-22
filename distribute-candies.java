f6faf08c002f9b932a63d19fbe1e7e068d0f031f
pokemon0121 <Delphox0121@gmail.com>
2017-12-21 23:52:42 -0800

    auto-update-file commit

--- distribute-candies.java
+++ distribute-candies.java
@@ -1,1 +1,15 @@
-test-string
+public class Solution {
+    public int distributeCandies(int[] candies) {
+        // n/2 candies, each person
+        // k kinds of candies.
+        // if k > n/2, so the answer is n/2
+        // else, answer = k
+        Set<Integer> s = new HashSet<>();
+        for (int c : candies) {
+            s.add(c);
+        }
+        int n = candies.length / 2;
+        return s.size() > n ? n : s.size();
+    }
+}
+​
