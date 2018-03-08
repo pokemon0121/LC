366c72a1851d8c25545f6ba1f25de3d8f5a8ea85
pokemon0121 <Delphox0121@gmail.com>
2018-03-07 18:55:26 -0800

    auto-update-file commit

--- closest-binary-search-tree-value-ii.java
+++ closest-binary-search-tree-value-ii.java
@@ -8,6 +8,8 @@
  * }
  */
 class Solution {
+    // O(n) to sorted list, O(logn) binary search, O(k) find k closest
+    /*
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         List<Integer> closestK = new ArrayList<>();
         if (root == null) return closestK;
@@ -19,7 +21,7 @@ class Solution {
             mid = start + (end - start) / 2;
             if (sorted.get(mid) > target) end = mid;
             else start = mid;
-        }
+        }
         // check start and end
         int closestIndex = Math.abs(sorted.get(start) - target) < Math.abs(sorted.get(end) - target) ? start : end;
         closestK.add(sorted.get(closestIndex));
@@ -31,15 +33,79 @@ class Solution {
                     closestK.add(sorted.get(i--));
                 else closestK.add(sorted.get(j++));
             }
-        }
+        }
         return closestK;
-    }
+    }
     
     private void toSortedArray(List<Integer> sorted, TreeNode root) {
         if (root == null) return;
         toSortedArray(sorted, root.left);
         sorted.add(root.val);
         toSortedArray(sorted, root.right);
+    }
+    */
+    // O(logn) + O(klogn)
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> closestK = new ArrayList<>();
+        if (root == null) return closestK;
+        TreeNode closest = root, cur = root;
+        while (cur != null) {
+            if (cur.val == target) {
+                closest = cur;
+                break;
+            }
+            if (Math.abs(cur.val - target) < Math.abs(closest.val - target))
+                closest = cur;
+            cur = target > cur.val ? cur.right : cur.left;
+        }
+        TreeNode pre = inorderPredecessor(root, closest);
+        TreeNode succ = inorderSuccessor(root, closest);
+        closestK.add(closest.val);
+        while ((pre != null || succ != null) && k-- > 1) {
+            if (pre == null) {
+                closestK.add(succ.val);
+                succ = inorderSuccessor(root, succ);
+            }
+            else if (succ == null) {
+                closestK.add(pre.val);
+                pre = inorderPredecessor(root, pre);
+            }
+            else {
+                if (Math.abs(pre.val - target) < Math.abs(succ.val - target)) {
+                    closestK.add(pre.val);
+                    pre = inorderPredecessor(root, pre);
+                }
+                else {
+                    closestK.add(succ.val);
+                    succ = inorderSuccessor(root, succ);
+                }
+            }
+        }
+        return closestK;
+    }
+    
+    private TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        TreeNode succ = null;
+        while (root != null) {
+            if (root.val > p.val) {
+                succ = root;
+                root = root.left;
+            }
+            else root = root.right;
+        }
+        return succ;
+    }
+    
+    private TreeNode inorderPredecessor(TreeNode root, TreeNode p) {
+        TreeNode pre = null;
+        while (root != null) {
+            if (root.val < p.val) {
+                pre = root;
+                root = root.right;
+            }
+            else root = root.left;
+        }
+        return pre;
     }
 }
 ​
